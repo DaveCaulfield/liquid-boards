8cd20072a71f0e469fe8c28770bd5e6e3e98207b
DaveCaulfield <dave.caulfield@gmail.com>
2022-10-25 06:31:30 +0000

    add sorthing JS and back to to icon/link to products page

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 # Create your views here.
@@ -19,9 +20,9 @@ def all_products(request):
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
-            if sortkey == 'name':
+            if sortkey == 'brand':
                 sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))
+                products = products.annotate(lower_name=Lower('brand'))
 
             if sortkey == 'category':
                 sortkey = 'category__name'
