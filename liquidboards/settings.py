c93344d52eb4cc132ba54203ab6cafefa36dc9d3
DaveCaulfield <dave.caulfield@gmail.com>
2022-10-26 19:48:59 +0000

    add checkout views and templates

--- liquidboards/settings.py
+++ liquidboards/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +67,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'liquidboards.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -80,8 +84,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'bag.contexts.bag_contents',
+                'django.template.context_processors.media',
+                'bag.contexts.bag_contents',               
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
