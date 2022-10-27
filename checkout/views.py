94199e73f462bd741c3c97cafafc37cd37b5719c
DaveCaulfield <dave.caulfield@gmail.com>
2022-10-27 15:46:00 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -18,6 +18,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LxXsPCrog7RoM46jl2VuOZD3DgPF7MLHQHaeq8qVt02U36FxmBP0YJwCOVHpceVMag3Uuvwm3QZfcmZZnHWFUej00SHyqb1mw',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
