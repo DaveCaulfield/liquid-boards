a486dd8d9553a00d710dcc8b8813a46310eea048
DaveCaulfield <dave.caulfield@gmail.com>
2022-10-28 15:47:10 +0000

    set debug to development

--- liquidboards/settings.py
+++ liquidboards/settings.py
@@ -28,8 +28,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = 'DEVELOPMENT' in os.environ
+
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['liquid-boards.herokuapp.com', 'localhost']
 
