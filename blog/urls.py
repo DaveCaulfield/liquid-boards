a387aa4de0ff70d9bac35307084a4c30231e6468
DaveCaulfield <dave.caulfield@gmail.com>
2022-11-13 21:55:16 +0000

    add blog detail view, template, url

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.BlogList.as_view(), name='blog'),     
-]
+    path('', views.BlogList.as_view(), name='blog'),
+    path('<slug:slug>/', views.BlogDetail.as_view(), name='blog_detail'),
+    
+    ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.views import generic, View
 from .models import Blog
 
 
@@ -10,7 +10,32 @@ class BlogList(generic.ListView):
     model = Blog
     queryset = Blog.objects.all().order_by('-created_on')
     template_name = 'blog/blog.html'
-    paginate_by = 6
+    paginate_by = 3
 
 
+class BlogDetail(View):
+    """
+    display blog post details 
+    """
 
+    def get(self, request, slug, *args, **kwargs):
+        """
+        get the blog post, check for comments and likes
+        """
+        queryset = Blog.objects
+        blog = get_object_or_404(queryset, slug=slug)
+        comments = blog.comments.order_by('created_on')
+        liked = False
+        if blog.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "blog/blog_detail.html",
+            {
+                "blog": blog,
+                "comments": comments,
+                "liked": liked,
+                
+            },
+        )
