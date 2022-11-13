264d087d5f2b55d05ab52ec4f5eca7b8c78ad66e
DaveCaulfield <dave.caulfield@gmail.com>
2022-11-13 17:33:40 +0000

    install summernote and register blog in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Blog, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Blog)
+class BlogAdmin(SummernoteModelAdmin):
+    """
+    Backend administration for Blog
+    """
+    summernote_fields = ('content')
+
+
+admin.site.register(Comment)
