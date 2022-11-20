70c855713bcdb3bafcf0894f9ed2572bbdd1ddb0
DaveCaulfield <dave.caulfield@gmail.com>
2022-11-20 16:33:09 +0000

    add logo image into navbar

--- home/views.py
+++ home/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    """ a View to return the index page """
+    """
+    a View to return the index page
+    """
     
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """
+    a View to return the about page
+    """
+    return render(request, 'home/about.html')
